c7b04efd0137fd974a0fb233154e1633e389e76a
Guruprasad Narwade <guruprasadnarwade1552@gmail.com>
2026-07-26 13:50:43 +0530

    Reorganize imports and server setup in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,14 @@
- import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+ import dotenv from "dotenv";
+dotenv.config();
 
+import express from "express";
+import cors from "cors";
 import carRoutes from "./routes/cars.js";
 import bookingRoutes from "./routes/bookings.js";
 import calendarRoutes from "./routes/calendar.js";
 import authRoutes from "./routes/auth.js";
 import adminRoutes from "./routes/adminRoutes.js";
-
-import "./config/db.js"; 
-
-dotenv.config();
+import "./config/db.js";
 
 const app = express();
 
@@ -30,32 +28,22 @@ app.use(cors({
 app.use(express.json());
 
 /* ROUTES */
-
 app.use("/api/cars", carRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/calendar", calendarRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/admin",adminRoutes);
+app.use("/api/admin", adminRoutes);
 
 /* ROOT */
-
 app.get("/", (req, res) => {
-
     res.json({
         success: true,
         message: "DriveNow Backend Running"
     });
-
 });
 
 /* START SERVER */
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-
-    console.log(
-        `🚗 Server Running On Port ${PORT}`
-    );
-
+    console.log(`🚗 Server Running On Port ${PORT}`);
 });
